e3043539ad0b5ff6cd6bc842e1ccc2f97b656db0
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-01-31 10:19:34 +0530

    Updated Files

--- AgumentedMatrix.py
+++ AgumentedMatrix.py
@@ -1,4 +1,3 @@
-# Define matrix A and vector B as lists
 A = [[1, 2, -1],
      [-2, 2, 3],
      [2, 0, 4]]
@@ -7,10 +6,8 @@ B = [[6],
      [3],
      [4]]
 
-# Construct the augmented matrix AB manually
 AB = [A[i] + B[i] for i in range(len(A))]
 
-# Print the augmented matrix
 print("Augmented matrix AB:")
 for row in AB:
     print(row)
